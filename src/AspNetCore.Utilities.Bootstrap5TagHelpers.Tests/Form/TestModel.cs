5ea7740e4ef0da78c6d4cbdccb2828afece14271
Mitchel Sellers <msellers@Iowacomputergurus.com>
2024-02-29 01:49:34 -0600

    Updated to support required label identification #26

--- src/AspNetCore.Utilities.Bootstrap5TagHelpers.Tests/Form/TestModel.cs
+++ src/AspNetCore.Utilities.Bootstrap5TagHelpers.Tests/Form/TestModel.cs
@@ -1,5 +1,10 @@
-﻿namespace ICG.AspNetCore.Utilities.Bootstrap5TagHelpers.Tests.Form;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICG.AspNetCore.Utilities.Bootstrap5TagHelpers.Tests.Form;
 public class TestModel
 {
     public string TextField { get; set; }
+
+    [Required]
+    public int? RequiredIntField { get; set; }
 }
